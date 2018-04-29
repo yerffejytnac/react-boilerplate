f804b7190ce71e9e21c9d4a8b693752d6dfe41e9
Jeffrey Canty <jeffrey.canty@casper.com>
2018-04-28 23:22:49 -0400

    Add HMR and configuration.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { AppContainer } from "react-hot-loader";
 import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById("root")
+  );
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept("./components/App", () => {
+    render(App);
+  });
+}
+
 registerServiceWorker();
